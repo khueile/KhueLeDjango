ac98b2cbe630c2b88bd4c62e87d0f7cbff455616
Khue Le <khuele@Khues-MacBook-Pro-2.local>
2021-08-12 15:19:14 -0400

    Class based views

--- Inventory App/inventory_app/productapp/urls.py
+++ Inventory App/inventory_app/productapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from productapp.views import list_products, product_detail,add_product,update_product
+from productapp.views import list_products, product_detail,add_product,update_product, delete_product
 
 app_name='productapp'
 urlpatterns=[
-    path('',list_products, name='list_products'),
-    path('<int:product_id>/', product_detail, name='product_detail'),
-    path('add/',add_product,name='add_product'),
-    path('<int:product_id>/update/', update_product, name='update_product'),
+    path('',list_products.as_view(), name='list_products'),
+    path('<int:pk>/', product_detail.as_view(), name='product_detail'),
+    path('add/',add_product.as_view(),name='add_product'),
+    path('<int:pk>/update/', update_product.as_view(), name='update_product'),
+    path('<int:pk>/delete/', delete_product.as_view(), name='delete_product'),
 ]
--- Inventory App/inventory_app/productapp/views.py
+++ Inventory App/inventory_app/productapp/views.py
@@ -4,21 +4,23 @@ from productapp.forms import productform
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, View
 
 # Create your views here.
-def list_products(request): # HTTPRequest  , web request
-    products = product.objects.all() # ' select * from products'
-    print(product)
-    return render(request, 'productapp/index.html', {'productkeys' : products}  ) #HttpResponse  web Response
+class list_products(ListView): # HTTPRequest  , web request
+    model=product
+    template_name= 'productapp/index.html'
+    context_object_name='productkeys'
 
-def product_detail(request, product_id):
-    productdetail = get_object_or_404(product, pk=int(product_id))
-    return render(request, 'productapp/detail.html', {'productdetail': productdetail})
+class product_detail(DetailView):
+    model=product
+    template_name= 'productapp/detail.html'
+    #context_object_name='productdetail'
 
-def add_product(request):
-    if request.method=='GET':
+class add_product(View):
+    def get(self, request):
         return render(request, 'productapp/form.html',{'productform' : productform})
-    else:
+    def post(self, request):
         form=productform(request.POST)
         if form.is_valid():
             form.save(commit=True)
@@ -26,16 +28,41 @@ def add_product(request):
         else:
             return render(request, 'productapp/form.html', {'productform': productform})
 
-def update_product(request,product_id):
-    updatecontent = get_object_or_404(product,pk=int(product_id))
-    if request.method=='GET':
+class update_product(View):
+    def get(self,request,**kwargs):
+        print(request)
+        #updatecontent = get_object_or_404(product, pk=int(product_id))
+        updatecontent = get_object_or_404(product,pk=kwargs['pk'])
         product_form = productform(instance=updatecontent)
         return render(request, 'productapp/update.html',{'productform':product_form})
-    else:
+    def post(self,request):
         product_form = productform(request.POST,instance=updatecontent)
         if product_form.is_valid():
             product_form.save(commit=True)
             return HttpResponseRedirect(reverse('products:list_products'))
         else:
             return render(request, 'productapp/update.html',{'productform':product_form})
+
+
+
+class delete_product(View):
+    def get(self,request,**kwargs):
+        context = {}
+        instance = get_object_or_404(product, pk=kwargs['pk'])
+        return render(request, "productapp/deleteinstance.html", context)
+
+    def post(self, request,**kwargs):
+        # dictionary for initial data with
+        # field names as keys
+        context = {}
+
+        # fetch the object related to passed id
+        instance = get_object_or_404(product, pk=kwargs['pk'])
+        instance.delete()
+        # after deleting redirect to
+
+        # home page
+        return HttpResponseRedirect(reverse('products:list_products'))
+
+
 
